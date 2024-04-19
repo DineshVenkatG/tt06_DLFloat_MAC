31b45d905ee5eac5e83382bacb11fa3e049da6df
DineshVenkatG <99543009+DineshVenkatG@users.noreply.github.com>
2024-04-19 16:29:35 +0530

    Update project.sv

--- src/project.sv
+++ src/project.sv
@@ -1,7 +1,7 @@
 `define default_netname none
 
 
-module tt_um_mac (
+module tt_um_dlfloatmac (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
